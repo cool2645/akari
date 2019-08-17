8bbbdb7b99db2950ab327e6d14630599ef00c419
Riko Sakurauchi <lijiahao99131@gmail.com>
2019-08-17 12:25:45 +0800

    :construction: merge twitter with posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -129,7 +129,7 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 
-  const _riko_twitter_data = graphql(`
+  const _riko_status_data = graphql(`
     query {
       allTwitterStatusesUserTimelineRikorikorilove {
         nodes {
@@ -142,6 +142,8 @@ exports.createPages = ({ graphql, actions }) => {
               }
             }
             source
+            favorite_count
+            retweet_count
           }
           id_str
           extended_entities {
@@ -150,35 +152,111 @@ exports.createPages = ({ graphql, actions }) => {
             }
           }
           source
+          created_at
+          retweet_count
+          favorite_count
         }
       }
     }
-  `).then(result => {
-    const perPage = 20
-    const pageCount = Math.ceil(
-      result.data.allTwitterStatusesUserTimelineRikorikorilove.nodes.length /
-        perPage
-    )
-    for (let i = 1; i <= pageCount; i++) {
-      createJSON({
-        path: `twitter/rikorikolove/${i}.json`,
-        context: {
-          nodes: result.data.allTwitterStatusesUserTimelineRikorikorilove.nodes.slice(
-            (i - 1) * 20,
-            i * 20
-          ),
-          pageInfo: {
-            pageCount,
-            perPage,
+  `).then(twitterResult => {
+    const twitterLen =
+      twitterResult.data.allTwitterStatusesUserTimelineRikorikorilove.nodes
+        .length
+    if (!twitterLen) return
+    const lastDate = new Date(
+      twitterResult.data.allTwitterStatusesUserTimelineRikorikorilove.nodes[
+        twitterLen - 1
+      ].created_at
+    ).toISOString()
+    return graphql(`
+      query {
+        allPost(
+          filter: {
+            is_public: { eq: true },
+            category: { slug: { eq: "2645lab" } },
+            author: { name: { eq: "梨子" } },
+            publish_at: { gte: "${lastDate}" }
           },
-        },
-      })
-    }
+          sort: {order: DESC, fields: publish_at}) {
+          nodes {
+            category {
+              slug
+            }
+            title
+            slug
+            childMdx {
+              excerpt(pruneLength: 300)
+            }
+            author {
+              avatar
+              name
+              email
+            }
+            publish_at
+            is_repost
+          }
+        }
+      }
+    `).then(blogResult => {
+      const blogLen = blogResult.data.allPost.nodes.length
+      const perPage = 20
+      const pageCount = Math.ceil((twitterLen + blogLen) / perPage)
+      let twitter_p = 0
+      let blog_p = 0
+      const pushBlog = nodes => {
+        blogResult.data.allPost.nodes[blog_p].t = 'post'
+        nodes.push(blogResult.data.allPost.nodes[blog_p++])
+      }
+      const pushTwitter = nodes => {
+        const twitter =
+          twitterResult.data.allTwitterStatusesUserTimelineRikorikorilove.nodes[
+            twitter_p
+          ]
+        twitter.t = 'twitter'
+        twitter.publish_at = new Date(twitter.created_at).toISOString()
+        nodes.push(twitter)
+        twitter_p++
+      }
+      for (let i = 1; i <= pageCount; i++) {
+        const nodes = []
+        for (let j = 0; j < perPage; j++) {
+          if (twitter_p >= twitterLen && blog_p >= blogLen) break
+          if (twitter_p >= twitterLen) {
+            pushBlog(nodes)
+            continue
+          }
+          if (blog_p >= blogLen) {
+            pushTwitter(nodes)
+            continue
+          }
+          if (
+            new Date(
+              twitterResult.data.allTwitterStatusesUserTimelineRikorikorilove.nodes[
+                twitter_p
+              ].created_at
+            ) > new Date(blogResult.data.allPost.nodes[blog_p].publish_at)
+          ) {
+            pushTwitter(nodes)
+          }
+          pushBlog(nodes)
+        }
+        createJSON({
+          path: `riko/status/${i}.json`,
+          context: {
+            nodes,
+            pageInfo: {
+              pageCount,
+              perPage,
+            },
+          },
+        })
+      }
+    })
   })
 
   return Promise.chain([
     _2645lab_index_pages,
     _2645lab_public_posts,
-    _riko_twitter_data,
+    _riko_status_data,
   ])
 }
