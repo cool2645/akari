cb6a76499a337876715b6e5ebe6ae7c900df1526
Riko Sakurauchi <lijiahao99131@gmail.com>
2019-08-19 01:00:46 +0800

    :construction: add hitokoto

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,16 @@
 
 // You can delete this file if you're not using it
 require('prismjs/themes/prism.css')
+
+if (
+  window.navigator.userAgent.indexOf('Firefox') === -1 &&
+  window.navigator.userAgent.indexOf('Chrome') !== -1
+) {
+  console.warn(
+    '嘿！还在用吃相难看的 Chrome/Chromium 浏览器吗，换 Firefox 试试吧'
+  )
+  console.warn(
+    '下载专为开发者定制的 Firefox Developer Edition：https://www.mozilla.org/zh-CN/firefox/developer/'
+  )
+  console.warn('让我们一同反对垄断，捍卫隐私安全，维护网络中立')
+}
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,12 +3,46 @@ import * as React from 'react'
 import Layout from '../components/2645lab/layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+interface NotFoundPageState {
+  hitokoto: string
+}
 
-export default NotFoundPage
+export default class extends React.Component<{}, NotFoundPageState> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      hitokoto: '',
+    }
+  }
+
+  public componentDidMount() {
+    this.getHitokoto()
+  }
+
+  public render() {
+    return (
+      <Layout>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>{this.state.hitokoto}</p>
+      </Layout>
+    )
+  }
+
+  private getHitokoto() {
+    fetch('https://v1.hitokoto.cn/?c=a')
+      .then(res => res.json())
+      .then(data => {
+        if (data.hitokoto) {
+          this.setState({
+            hitokoto: data.hitokoto,
+          })
+        } else throw data
+      })
+      .catch(() => {
+        this.setState({
+          hitokoto: '网页咕咕咕了',
+        })
+      })
+  }
+}
