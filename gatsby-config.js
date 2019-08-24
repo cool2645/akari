5c95f43a0cd4705f98be451905c5cd79297cac87
Riko Sakurauchi <lijiahao99131@gmail.com>
2019-08-25 03:17:35 +0800

    :bento: favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,13 +47,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `2645实验室`,
+        short_name: `2645实验室`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ee8e00`,
+        theme_color: `#ee8e00`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
